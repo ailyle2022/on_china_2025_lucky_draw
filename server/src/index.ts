7f05fd4b697876a1f254f77d123b147d6c3fb5aa
Aily Le <aily.le@on-running.com>
2024-12-16 16:26:13 +0800

    数据库初始化和调用

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, TypeScript with Node.js!");
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const prizes = await prisma.prize.findMany();
+  console.log(prizes);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
